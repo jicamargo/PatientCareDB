a0ce3ac605ad95ab2989ea8d4689bc6002688c53
jic <ji_camargo@yahoo.com>
2023-07-26 15:59:33 -0500

    add code to create 6 indexes in schema_based_on_diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -54,3 +54,17 @@ CREATE TABLE medical_history_treatments (
     FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
     PRIMARY KEY (treatment_id, medical_history_id)
 );
+
+-- Create indexes to improve performance
+
+CREATE INDEX idx_medical_histories_patient_id ON medical_histories (patient_id);
+
+CREATE INDEX idx_invoices_medical_history_id ON invoices (medical_history_id);
+
+CREATE INDEX idx_invoice_items_invoice_id ON invoice_items (invoice_id);
+
+CREATE INDEX idx_invoice_items_treatment_id ON invoice_items (treatment_id);
+
+CREATE INDEX idx_medical_history_treatments_treatment_id ON medical_history_treatments (treatment_id);
+
+CREATE INDEX idx_medical_history_treatments_medical_history_id ON medical_history_treatments (medical_history_id);
